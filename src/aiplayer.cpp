5d843d5d3a726bcd9453cd4c8583849a69cfd608
Jackson Fu <diedirac@gmail.com>
2021-06-02 22:25:12 +0800

    Add files via upload
    x

--- src/aiplayer.cpp
+++ src/aiplayer.cpp
@@ -241,6 +241,109 @@ bool AIPlayer::getMove(ChessBoard & board, Move & move) const
 	}
 }
 
+bool AIPlayer::getMove(ChessBoard & board, Move & move, unsigned long long* nodeCount) const
+{
+	list<Move> regulars, nulls;
+	vector<Move> candidates;
+	bool quiescent = false;
+	int best, tmp; //// tmp can be seen as temporary `this.advantage`
+
+	// first assume we are loosing
+	best = -KING_VALUE;
+
+	// get all moves
+	board.getMoves(this->color, regulars, regulars, nulls);
+
+	// execute maintenance moves  //// clear marks of passant on pawns
+	for(list<Move>::iterator it = nulls.begin(); it != nulls.end(); ++it)
+		board.move(*it);
+
+	//// parallelization starts
+	pthread_t dummy;
+	AIPlayer dummyAIPlayer(this->color, this->search_depth);
+	std::vector<pthread_t> threads(NUM_THREADS, dummy);
+	std::vector<Arguments> threadArgs(NUM_THREADS,
+					  Arguments(0, //// threadId
+						    dummyAIPlayer,
+						    board,
+						    regulars,
+						    candidates,
+						    this->color,
+						    this->search_depth,
+						    best,
+						    0, //// tmp
+						    quiescent)
+					 );
+
+	for(int tid = 1; tid < NUM_THREADS; tid++){
+		(threadArgs[tid]).threadId = tid; //// assign the correct threadId
+		pthread_create(&(threads[tid]), nullptr, &parallel_search, (void*)(&(threadArgs[tid])));
+	}
+
+	//// main thread
+	parallel_search((void*)(&(threadArgs[0])));
+
+	//// start: combine the results of individual threads
+	int threadBest = (threadArgs[0]).best;
+	std::vector<Move> threadCandidates;
+	for(int tid = 1; tid < NUM_THREADS; tid++){
+		pthread_join(threads[tid], nullptr);
+		if(threadArgs[tid].best > threadBest)
+			threadBest = threadArgs[tid].best;
+	}
+	for(int tid = 0; tid < NUM_THREADS; tid++){
+		if(threadArgs[tid].best == threadBest){
+			//// append the candidates of threadId to the end of `threadCandidates`
+			threadCandidates.insert(threadCandidates.end(), 
+					        threadArgs[tid].candidates.begin(), 
+					        threadArgs[tid].candidates.end()); 
+		}
+	} //// end: combine the results of individual threads
+
+	// loop over all moves  //// and find the candidate moves
+	for(list<Move>::iterator it = regulars.begin(); it != regulars.end(); ++it)
+	{
+		// execute move
+		board.move(*it);
+
+		// check if own king is vulnerable now
+		if(!board.isVulnerable((this->color ? board.black_king_pos : board.white_king_pos), this->color)) {
+
+			if((*it).capture != EMPTY) {
+				quiescent = true;
+			}
+
+			// recursion
+			tmp = -evalAlphaBeta(board, TOGGLE_COLOR(this->color), this->search_depth - 1, -WIN_VALUE, -best, quiescent);
+			if(tmp > best) {
+				best = tmp;
+				candidates.clear();
+				candidates.push_back(*it);
+			}
+			else if(tmp == best) {
+				candidates.push_back(*it);
+			}
+		}
+
+		// undo move and inc iterator
+		board.undoMove(*it);
+	}
+
+	// undo maintenance moves
+	for(list<Move>::iterator it = nulls.begin(); it != nulls.end(); ++it)
+		board.undoMove(*it);
+
+	// loosing the game?
+	if(best < -WIN_VALUE) {
+		return false;
+	}
+	else {
+		// select random move from candidate moves
+		move = candidates[rand() % candidates.size()];
+		return true;
+	}
+}
+
 int AIPlayer::evalAlphaBeta(ChessBoard & board, int color, int search_depth, int alpha, int beta, bool quiescent) const
 {    //// return the ADVANTAGE of `color` just before `color` moves
 	list<Move> regulars, nulls;
